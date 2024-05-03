abd2d0e982240ac3b763715b0d1b57974369aae7
Miguel Torrealba <devemtv@gmail.com>
2024-05-03 15:50:26 +0200

    config: update environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = 'https://maigueldev.github.io/qr-generator';
+
+let BASE_URL = LOCALHOST_URL;
+let CONFIG = {};
+const isProduction = process.env.DEVELOPMENT_MODE === 'PRODUCTION';
+
+if (isProduction) {
+    CONFIG = {
+        site: LIVE_URL,
+        base: BASE_URL,
+    }
+}
+
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://maigueldev.github.io',
-    base: 'qr-generator',
-});
+export default defineConfig(CONFIG);
